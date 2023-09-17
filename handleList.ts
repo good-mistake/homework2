7f59a8adaf9e8f1fa9ce83f82fcf6c01d5fb520f
good-mistake <devillmk88@gmail.com>
2023-09-17 17:16:36 +0330

    s

---
+++ handleList.ts
@@ -0,0 +1,16 @@
+const user = {
+  userName: [],
+  UserNumber: [],
+};
+function contact(i: string, x: number) {
+  for (let z = 0; z < 999; z++) {
+    let name = prompt(`Please enter a name:example(${i}) =  `);
+    let number = parseInt(prompt(`Please enter a number:example(${x}) =  `));
+    user.userName.push(name);
+    user.UserNumber.push(number);
+
+    console.log(user);
+  }
+  return user;
+}
+contact("reza", 9177777777);
